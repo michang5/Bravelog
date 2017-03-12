bc4060c22422dacf1ceb0c4b8313cf6b7a5cb2a9
MiChang <immi5.tw@gmail.com>
2017-03-13 00:53:36 +0800

    風格調整 / 首頁 / 個別賽事搜尋結果 / 搜尋結果頁

--- js/script.js
+++ js/script.js
@@ -2,9 +2,32 @@ $(function () {
 	$('[href="#top"], [href="#sport-1"], [href="#sport-2"], [href="#sport-3"]').click(function () {
 		if ($(this).attr('href') == '#') {} else {
 			$('html, body').animate({
-				scrollTop: $($(this).attr('href')).offset().top - 35
+				scrollTop: $($(this).attr('href')).offset().top - 50
 			}, 500);
 			return false;
 		}
 	});
 });
+$(".carousel").swipe({
+  swipe: function(event, direction, distance, duration, fingerCount, fingerData) {
+
+    if (direction == 'left') $(this).carousel('next');
+    if (direction == 'right') $(this).carousel('prev');
+
+  },
+  allowPageScroll:"vertical"
+
+});
+window.onscroll = function(){
+	var target = document.body;	
+	var top_btn = document.querySelector('#top-btn');	
+	var y = target.scrollTop,
+    offset = 200; 
+        
+	if( y >= offset){
+		top_btn.setAttribute('class', 'fixed');
+      
+	} else {
+		top_btn.removeAttribute('class');
+	}
+}
