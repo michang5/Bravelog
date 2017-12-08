2ef467c2c618cbaf9d5568a06532cdda9cdd332b
MiChang <immi5.tw@gmail.com>
2017-12-08 18:27:59 +0800

    account page

--- js/forms.js
+++ js/forms.js
@@ -9,35 +9,36 @@ $(function () {
 });
 
 var inputPass1 = document.getElementById('pass1'),
-            icon1 = document.getElementById('icon1');
+    icon1 = document.getElementById('icon1');
 
-        icon1.onclick = function() {
+icon1.onclick = function () {
 
-            if (inputPass1.className == 'active') {
-                inputPass1.setAttribute('type', 'text');
-                icon1.className = 'fa fa-eye';
-                inputPass1.className = '';
+    if (inputPass1.className == 'active') {
+        inputPass1.setAttribute('type', 'text');
+        icon1.className = 'fa fa-eye';
+        inputPass1.className = '';
 
-            } else {
-                inputPass1.setAttribute('type', 'password');
-                icon1.className = 'fa fa-eye-slash';
-                inputPass1.className = 'active';
-            }
-        }
+    } else {
+        inputPass1.setAttribute('type', 'password');
+        icon1.className = 'fa fa-eye-slash';
+        inputPass1.className = 'active';
+    }
+}
 
-        var inputPass2 = document.getElementById('pass2'),
-            icon2 = document.getElementById('icon2');
+var inputPass2 = document.getElementById('pass2'),
+    icon2 = document.getElementById('icon2');
 
-        icon2.onclick = function() {
+icon2.onclick = function () {
 
-            if (inputPass2.className == 'active') {
-                inputPass2.setAttribute('type', 'text');
-                icon2.className = 'fa fa-eye';
-                inputPass2.className = '';
+    if (inputPass2.className == 'active') {
+        inputPass2.setAttribute('type', 'text');
+        icon2.className = 'fa fa-eye';
+        inputPass2.className = '';
 
-            } else {
-                inputPass2.setAttribute('type', 'password');
-                icon2.className = 'fa fa-eye-slash';
-                inputPass2.className = 'active';
-            }
-        }
+    } else {
+        inputPass2.setAttribute('type', 'password');
+        icon2.className = 'fa fa-eye-slash';
+        inputPass2.className = 'active';
+    }
+}
+    
